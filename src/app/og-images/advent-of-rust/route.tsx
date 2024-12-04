b2531de7acb85d80d681a051fedfec520bc3fb5c
Meysam Mahmoudi <meisamdev@gmail.com>
2024-12-04 09:30:31 +0330

    Show advent of Rust title info as OG image

--- src/app/og-images/advent-of-rust/route.tsx
+++ src/app/og-images/advent-of-rust/route.tsx
@@ -1,15 +1,32 @@
 import { ImageResponse } from '@vercel/og'
+import { NextRequest } from 'next/server';
+import { load } from 'cheerio';
 
 export const config = {
     runtime: 'experimental-edge',
 }
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+    const { searchParams } = req.nextUrl;
+    const day = searchParams.get("key");
+    const pageUrl = `https://www.rustfinity.com/practice/rust/challenges/${day}/description`;
+    const response = await fetch(pageUrl);
+    const html = await response.text();
+
+    // Load the HTML into cheerio for parsing
+    const $ = load(html);
+
+    const title = $('meta[property="og:title"]').attr('content');
+    const description = $('meta[property="og:description"]').attr('content');
+
     return new ImageResponse(
         (
-            <div>
-                hello world!
+            <div tw='flex flex-col'>
+                <p> {title} </p>
+                <p> {description} </p>
             </div>
-        )
+        ), {
+        debug: true
+    }
     );
 };
