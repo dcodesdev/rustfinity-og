1706e65702e952e9df64a8389b994b3779f1f282
Meysam Mahmoudi <meisamdev@gmail.com>
2024-12-06 23:12:42 +0330

    Change username OG layout

--- src/app/og-images/Username/route.tsx
+++ src/app/og-images/Username/route.tsx
@@ -8,6 +8,10 @@ export async function GET(req: NextRequest) {
     const fullName = searchParams.get("fullName");
     const imageUrl = searchParams.get("imageUrl") ?? "";
 
+    console.log(username);
+    console.log(fullName);
+    console.log(imageUrl);
+
     const geistBold = fetch(new URL(siteUrl + 'Geist/ttf/Geist-Bold.ttf')).then((res) => res.arrayBuffer(),);
     const geistBoldFontData = await geistBold;
 
@@ -16,21 +20,40 @@ export async function GET(req: NextRequest) {
 
     return new ImageResponse(
         (
-            <div tw="h-full w-full rounded-md flex items-center justify-top bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden flex-col" style={{ backgroundColor: 'black' }}>
+            <div tw="h-full w-full rounded-md flex items-center justify-top bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden flex-row" style={{ backgroundColor: 'black' }}>
 
-                {/*eslint-disable-next-line @next/next/no-img-element*/}
-                <img alt=''
-                    tw='h-[50] w-[50]  mt-[70] mb-[40]'
-                    src={imageUrl}
-                />
+                <div tw="relative text-sm font-medium leading-6 flex ml-[200] mt-[150]">
+                    <div tw="rounded-lg p-4 flex">
+                        <div tw="static p-4 h-32 flex">
 
-                <h1 tw="text-6xl md:text-7xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 flex" style={{ backgroundImage: "linear-gradient(#fafafa, #a3a3a3)", color: "transparent", backgroundClip: "text", fontWeight: 700 }}>
-                    {fullName}
-                </h1>
+                            <div tw="absolute bottom-0 right-0 lg rounded-lg p-4 flex">
+                                {/*eslint-disable-next-line @next/next/no-img-element*/}
+                                <img alt=''
+                                    tw='h-[85] w-[85] rounded-full'
+                                    src={imageUrl}
+                                />
+                            </div>
+                            <p tw="flex">
+                                {/*eslint-disable-next-line @next/next/no-img-element*/}
+                                <img alt=''
+                                    tw='h-[35] w-[35] '
+                                    src={`${siteUrl}/rustfinity-logo.png`}
+                                />
+                            </p>
 
-                <h1 tw="text-4xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 flex" style={{ backgroundImage: "linear-gradient(#fafafa, #a3a3a3)", color: "transparent", backgroundClip: "text", fontWeight: 400 }}>
-                    {username}
-                </h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div tw='flex flex-col'>
+                    <h1 tw="text-7xl text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 flex text-wrap mr-[450]" style={{ backgroundImage: "linear-gradient(#fafafa, #a3a3a3)", color: "transparent", backgroundClip: "text", fontWeight: 700 }}>
+                        {fullName}
+                    </h1>
+
+                    <h1 tw="text-4xl text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 flex" style={{ backgroundImage: "linear-gradient(#fafafa, #a3a3a3)", color: "transparent", backgroundClip: "text", fontWeight: 400 }}>
+                        {username}
+                    </h1>
+                </div>
 
                 <div tw="absolute h-full w-full inset-0 [mask-size:40px] [mask-repeat:no-repeat] flex items-center justify-center">
                     {/*eslint-disable-next-line @next/next/no-img-element*/}
@@ -44,7 +67,7 @@ export async function GET(req: NextRequest) {
         ), {
         width: 1200,
         height: 630,
-        debug: process.env.NODE_ENV == "development",
+        //debug: process.env.NODE_ENV == "development",
         fonts: [
             {
                 name: 'GeistVF',
